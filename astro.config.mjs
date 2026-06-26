02edf322ea832bf3e3dcc3df83cbeff9bc878a35
abbymawhinney <abby.mawhinney@hotmail.co.uk>
2026-06-26 17:15:15 +0100

    Set build.format to "file" to resolve CF trailing slash issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -51,6 +51,9 @@ export default defineConfig({
   site: config.site.base_url ? config.site.base_url : "http://examplesite.com",
   base: config.site.base_path ? config.site.base_path : "/",
   trailingSlash: config.site.trailing_slash ? "always" : "never",
+  build: {
+    format: "file",
+  },
   image: { service: sharp() },
   vite: { plugins: [tailwindcss()] },
   fonts: fontsConfig,
